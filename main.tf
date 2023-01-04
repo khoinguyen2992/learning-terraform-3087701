6f7eb632afaf68f051bd4d54121c1f605c7d76b2
minhkhoi.nguyen <minhkhoi.nguyen@grabtaxi.com>
2023-01-04 15:57:43 +0800

    asg

--- main.tf
+++ main.tf
@@ -31,15 +31,19 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+module "blog_autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.5.2"
 
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+  name = "blog"
 
-  tags = {
-    Name = "Learning Terraform"
-  }
+  min_size            = 1
+  max_size            = 2
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+  instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
 }
 
 module "blog_alb" {
@@ -60,12 +64,6 @@ module "blog_alb" {
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id = aws_instance.blog.id
-          port      = 80
-        }
-      }
     }
   ]
 
